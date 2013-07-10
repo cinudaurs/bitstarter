d91d14cd1cdfb57637316b5eb81281feff8af7dc
cinudaurs <shriniva@gmail.com>
2013-07-10 16:52:24 -0400

    read from index.html added

--- web.js
+++ web.js
@@ -1,10 +1,22 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+app.get('/', function(request, response){
+  
+  var content;
+  fs = require('fs')
+  fs.readFile('./index.html', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  buf = new Buffer(data);
+  len = buf.write(data.toString());
+  content = buf.toString('utf8', 0, len);
+  
 });
+  response.send(content);
+});
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
